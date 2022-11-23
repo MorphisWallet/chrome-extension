947cbe8289bc69616095161888077a770a913560
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-23 19:39:59 +0800

    feat(): fork Mysten core

---
+++ src/shared/cryptography/mnemonics.test.ts
@@ -0,0 +1,41 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Base64DataBuffer, Ed25519Keypair } from '@mysten/sui.js'
+
+import {
+  generateMnemonicsAndKeypair,
+  getKeypairFromMnemonics,
+  normalizeMnemonics,
+} from './mnemonics'
+
+describe('mnemonics', () => {
+  it('generate mnemonics', () => {
+    const [mnemonics, keypair] = generateMnemonicsAndKeypair()
+    expect(mnemonics.split(' ').length).toBe(12)
+    const parsedKeypair = getKeypairFromMnemonics(mnemonics, -1)
+    expect(parsedKeypair.publicKey).toEqual(keypair.publicKey)
+    expect(parsedKeypair.secretKey).toEqual(keypair.secretKey)
+  })
+
+  it('normalize', () => {
+    expect(normalizeMnemonics(' Almost a Seed    Phrase')).toEqual(
+      'almost a seed phrase'
+    )
+  })
+
+  it('parse mnemonics', () => {
+    const keypairData = getKeypairFromMnemonics(
+      'Shoot island position soft burden budget tooth cruel issue economy destroy Above'
+    )
+
+    const keypair = new Ed25519Keypair(keypairData)
+
+    expect(new Base64DataBuffer(keypairData.secretKey).toString()).toEqual(
+      'uYSGvJ/dr9US/nRRyChSQ0tBsMemg+Az8WVypeS32lMzEeahmtwcBqUJ5b9GTp1OzHMDnzEayUC7EYlvgvtlMw=='
+    )
+    expect(keypair.getPublicKey().toBase64()).toEqual(
+      'MxHmoZrcHAalCeW/Rk6dTsxzA58xGslAuxGJb4L7ZTM='
+    )
+  })
+})
---
+++ src/types/assets.d.ts
@@ -0,0 +1,27 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
+
+declare module '*.gif' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
