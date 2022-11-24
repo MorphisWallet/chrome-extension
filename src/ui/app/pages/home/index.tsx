d0304bfb617fa39cce7af87bc94cf172f42cb1b8
AlexanderZhao <zhaomengzhe@gmail.com>
2022-11-24 17:44:25 +0800

    feat(): dev Home and Welcome pages

---
+++ src/ui/app/pages/home/index.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
+import { of, filter, switchMap, from, defer, repeat } from 'rxjs'
+
+import { Layout } from '_app/layouts'
+import { Loading } from '_components/loading'
+
+import { useAppDispatch, useInitializedGuard, useLockedGuard } from '_hooks'
+
+import { fetchAllOwnedAndRequiredObjects } from '_redux/slices/sui-objects'
+
+const POLL_SUI_OBJECTS_INTERVAL = 4000
+
+export const Home = () => {
+  const dispatch = useAppDispatch()
+  const initChecking = useInitializedGuard(true)
+  const lockedChecking = useLockedGuard(false)
+
+  const [visibility, setVisibility] = useState(
+    document?.visibilityState || null
+  )
+
+  const guardChecking = initChecking || lockedChecking
+
+  useEffect(() => {
+    const callback = () => {
+      setVisibility(document.visibilityState)
+    }
+    callback()
+
+    document.addEventListener('visibilitychange', callback)
+
+    return () => {
+      document.removeEventListener('visibilitychange', callback)
+    }
+  }, [])
+
+  useEffect(() => {
+    const sub = of(guardChecking || visibility === 'hidden')
+      .pipe(
+        filter((paused) => !paused),
+        switchMap(() =>
+          defer(() => from(dispatch(fetchAllOwnedAndRequiredObjects()))).pipe(
+            repeat({ delay: POLL_SUI_OBJECTS_INTERVAL })
+          )
+        )
+      )
+      .subscribe()
+    return () => sub.unsubscribe()
+  }, [guardChecking, visibility, dispatch])
+
+  return (
+    <Layout>
+      <Loading loading={guardChecking}>
+        <Outlet />
+      </Loading>
+    </Layout>
+  )
+}
---
+++ src/ui/app/pages/welcome/index.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+import { Button } from '_components/button'
+
+import Logo from '_assets/icons/logo.svg'
+
+export const Welcome = () => {
+  return (
+    <div className="flex flex-col grow justify-center p-10 font-medium">
+      <a
+        href="https://morphiswallet.com/#features"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Logo height={55} width={57} className="mb-6" />
+      </a>
+      <p className="text-4xl leading-[48px] mb-3">Morphis</p>
+      <p className="text-lg text-[#929294] leading-6 mb-8">
+        A friendly crypto wallet for your web3 journey
+      </p>
+      <Link to="/" className="w-full mb-4">
+        <Button variant="contained">Create a new wallet</Button>
+      </Link>
+      <Link to="/" className="w-full">
+        <Button variant="outlined">I already have a wallet</Button>
+      </Link>
+    </div>
+  )
+}
