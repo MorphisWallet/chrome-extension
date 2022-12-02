d2cea52da57a9ca0c77f8898deca6b5b59e0ce23
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-02 14:52:20 +0800

    feat(): add TxLink component

---
+++ src/ui/app/components/tx_link/index.tsx
@@ -0,0 +1,68 @@
+import { useMemo } from 'react'
+import cl from 'classnames'
+
+import { useAppSelector } from '_hooks'
+
+import { activeAccountSelector } from '_redux/slices/account'
+
+import { getAddressUrl, getObjectUrl, getTransactionUrl } from './utils'
+import { API_ENV } from '_app/ApiProvider'
+import { ExplorerLinkType, TxObjectTypes } from './type'
+
+type TxLinkProps = TxObjectTypes &
+  React.DOMAttributes<HTMLAnchorElement> & {
+    className: string
+  }
+
+function useAddress(props: TxLinkProps) {
+  const { type } = props
+  const isAddress = type === ExplorerLinkType.address
+  const isProvidedAddress = isAddress && !props.useActiveAddress
+  const activeAddress = useAppSelector(activeAccountSelector)
+  return isProvidedAddress ? props.address : activeAddress
+}
+
+export const TxLink = (props: TxLinkProps) => {
+  const { children, className, type } = props
+
+  const selectedApiEnv = useAppSelector(({ app }) => app.apiEnv)
+  const address = useAddress(props)
+
+  const notCustomRPC = selectedApiEnv !== API_ENV.customRPC
+  const objectID = type === ExplorerLinkType.object ? props.objectID : null
+  const transactionID =
+    type === ExplorerLinkType.transaction ? props.transactionID : null
+
+  const explorerHref = useMemo(() => {
+    switch (type) {
+      case ExplorerLinkType.address:
+        return address && notCustomRPC && getAddressUrl(address, selectedApiEnv)
+      case ExplorerLinkType.object:
+        return (
+          objectID && notCustomRPC && getObjectUrl(objectID, selectedApiEnv)
+        )
+      case ExplorerLinkType.transaction:
+        return (
+          transactionID &&
+          notCustomRPC &&
+          getTransactionUrl(transactionID, selectedApiEnv)
+        )
+    }
+  }, [type, address, notCustomRPC, selectedApiEnv, objectID, transactionID])
+
+  const disableLink = selectedApiEnv === API_ENV.customRPC || !explorerHref
+
+  return (
+    <a
+      className={cl([
+        disableLink && 'pointer-events-none cursor-not-allowed',
+        className,
+      ])}
+      href={explorerHref || undefined}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {children}
+    </a>
+  )
+}
