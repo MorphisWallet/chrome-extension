818552240874cca407a32b71bc9622189b1caff4
AlexanderZhao <zhaomengzhe@gmail.com>
2022-12-14 11:42:31 +0800

    feat(): dev seed phrase Confirm component

---
+++ src/ui/app/pages/settings/sap/components/confirm.tsx
@@ -0,0 +1,88 @@
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+import { Button, Input, Checkbox } from '_app/components'
+
+import { useAppDispatch } from '_hooks'
+
+import { unlockWallet } from '_redux/slices/wallet'
+
+import { passwordValidation } from '_app/utils/validation'
+
+type ConfirmModalProps = {
+  subject: string
+  onSuccess: () => void
+}
+
+type Fields = {
+  password: string
+  agreement: boolean
+}
+
+const Confirm = ({ subject, onSuccess }: ConfirmModalProps) => {
+  const dispatch = useAppDispatch()
+
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    setFieldError,
+  } = useFormik<Fields>({
+    initialValues: {
+      password: '',
+      agreement: false,
+    },
+    validationSchema: Yup.object().shape({
+      password: passwordValidation,
+      agreement: Yup.boolean().required().oneOf([true]),
+    }),
+    onSubmit: async ({ password }) => {
+      try {
+        await dispatch(unlockWallet({ password })).unwrap()
+        onSuccess()
+      } catch (e) {
+        setFieldError('password', (e as Error).message || 'Incorrect password')
+      }
+    },
+  })
+
+  return (
+    <form className="flex flex-col grow" onSubmit={handleSubmit}>
+      <p className="mb-12 text-sm">{`Enter your password to show the ${subject}. Never share the ${subject}. Anyone with these words will have full access to your wallet. `}</p>
+      <div className="flex flex-col grow justify-center">
+        <label htmlFor="password" className="mb-2">
+          Password
+        </label>
+        <Input
+          id="password"
+          name="password"
+          value={values.password}
+          error={touched.password && errors.password}
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className="mb-3"
+        />
+        <Checkbox
+          id="agreement"
+          name="agreement"
+          checked={values.agreement}
+          onChange={handleChange}
+          className="mb-4 !items-start"
+          inputClassName='mt-0.5'
+          error={touched.agreement && !!errors.agreement}
+          label="I will not share my seed phrase with anyone, including Morphis."
+        />
+      </div>
+      <div className="grow" />
+      <Button loading={isSubmitting}>Confirm</Button>
+    </form>
+  )
+}
+
+export default Confirm
--- src/ui/app/pages/settings/sap/seed_phrase/index.tsx
+++ src/ui/app/pages/settings/sap/seed_phrase/index.tsx
@@ -1,79 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import ReactTooltip from 'react-tooltip'
-import copy from 'copy-to-clipboard'
 
 import Layout from '_app/layouts'
-import { IconWrapper, Loading, Alert, toast } from '_app/components'
-
-import { useAppDispatch } from '_hooks'
-
-import { loadEntropyFromKeyring } from '_redux/slices/account'
-
-import { MNEMONIC_LENGTH } from '_src/shared/constants'
-import { entropyToMnemonic, toEntropy } from '_shared/utils/bip39'
+import { IconWrapper, SeedPhraseCard } from '_app/components'
+import Confirm from '../components/confirm'
 
 import ArrowShort from '_assets/icons/arrow_short.svg'
-import CopyIcon from '_assets/icons/copy.svg'
-
-let copyTimeout: ReturnType<typeof setTimeout>
 
 const SeedPhrasePage = () => {
-  const dispatch = useAppDispatch()
-
-  const [mnemonicLoading, setMnemonicLoading] = useState(false)
-  const [mnemonic, setMnemonic] = useState<string | null>(null)
-  const [tipVisible, setTipVisibility] = useState(false)
-
-  const onInit = async () => {
-    setMnemonicLoading(true)
-    try {
-      const raw = await dispatch(loadEntropyFromKeyring({})).unwrap()
-      const entropy = toEntropy(raw)
-      const mnemonic = entropyToMnemonic(entropy)
-      setMnemonic(mnemonic)
-    } catch (e) {
-      console.warn(e)
-      toast({
-        type: 'error',
-        message: `Fail to load mnemonics, ${e}`,
-      })
-    } finally {
-      setMnemonicLoading(false)
-    }
-  }
-
-  const onCopy = () => {
-    if (!mnemonic) {
-      return
-    }
-
-    const copyRes = copy(mnemonic)
-    if (copyRes) {
-      clearTimeout(copyTimeout)
-      setTipVisibility(true)
-
-      copyTimeout = setTimeout(() => {
-        setTipVisibility(false)
-      }, 3000)
-      return
-    }
-
-    toast({
-      type: 'error',
-      message: 'Fail to copy, try to copy manually',
-      containerId: 'initialize-toast',
-    })
-  }
-
-  useEffect(() => {
-    onInit()
-  }, [])
-
-  const mnemonicArray = mnemonic?.split(' ')
+  const [showConfirm, setShowConfirm] = useState(true)
 
   return (
-    <Layout>
+    <Layout showNav={!showConfirm}>
       <div className="flex flex-col grow font-medium px-6 pt-4 pb-6">
         <div className="mb-6 text-xl text-center font-bold relative">
           Seed Phrase
@@ -83,52 +21,14 @@ const SeedPhrasePage = () => {
             </IconWrapper>
           </Link>
         </div>
-        <Loading loading={mnemonicLoading}>
-          {!!mnemonicArray?.length && (
-            <div className="flex flex-col grow justify-center">
-              <div className="grid grid-cols-3 grid-rows-4 px-3.5 py-3 mb-2.5 gap-x-1 gap-y-4 border border-[#7e7e7e] rounded-[10px]">
-                {Array(MNEMONIC_LENGTH)
-                  .fill(null)
-                  .map((_, i) => (
-                    <div key={i} className="flex text-[13px]">
-                      <span className="text-[#c8c8c8] w-6 shrink-0 select-none">
-                        {String(i + 1).padStart(2, '0')}
-                      </span>
-                      <span
-                        data-tip={mnemonicArray[i]}
-                        data-for="mnemonic-tooltip"
-                        className="truncate"
-                      >
-                        {mnemonicArray[i]}
-                      </span>
-                    </div>
-                  ))}
-                <ReactTooltip
-                  id="mnemonic-tooltip"
-                  place="top"
-                  effect="solid"
-                  padding="4px 8px"
-                  delayHide={500}
-                  className="!pointer-events-auto before:hidden hover:!visible hover:!opacity-100"
-                />
-              </div>
-              <div className="self-start relative">
-                <IconWrapper onClick={onCopy}>
-                  <CopyIcon height={14} width={14} className="mr-2" />
-                  Copy
-                </IconWrapper>
-                {tipVisible && (
-                  <Alert
-                    type="success"
-                    className="absolute justify-center h-[30px] w-[166px] left-[64px] top-[-7px] shadow-[0_2px_6px_0_rgba(165,161,161,0.25)] animate-fade-in"
-                  >
-                    Copied to clipboard
-                  </Alert>
-                )}
-              </div>
-            </div>
-          )}
-        </Loading>
+        {showConfirm ? (
+          <Confirm
+            subject="Seed Phrase"
+            onSuccess={() => setShowConfirm(false)}
+          />
+        ) : (
+          <SeedPhraseCard />
+        )}
       </div>
     </Layout>
   )
