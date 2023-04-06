fc5bb801c87d49d0fab314de74105e79d2d9c3e3
AlexanderZhao <zhaomengzhe@gmail.com>
2023-04-06 17:38:37 +0800

    refactor(): refactor wallet management ui

--- src/ui/app/pages/settings/wallet_management/index.tsx
+++ src/ui/app/pages/settings/wallet_management/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import ReactTooltip from 'react-tooltip'
 import cl from 'classnames'
@@ -9,30 +10,33 @@ import { IconWrapper, Button, Avatar, toast } from '_app/components'
 import { useAccountsMeta, useActiveAddress } from '_hooks'
 import { useAccounts } from '_src/ui/app/hooks/useAccounts'
 import { useDeriveNextAccountMutation } from '_src/ui/app/hooks/useDeriveNextAccountMutation'
-
-import {} from '_redux/slices/account'
+import { useBackgroundClient } from '_src/ui/app/hooks/useBackgroundClient'
 
 import ArrowShort from '_assets/icons/arrow_short.svg'
 import ImportIcon from '_assets/icons/import.svg'
 import SettingsIcon from '_assets/icons/settings.svg'
 import AddIcon from '_assets/icons/add.svg'
-import { useEffect } from 'react'
 
 type AccountSelectProps = {
   address: SuiAddress
-  onSetActiveAccount: (address: SuiAddress) => void
 }
 
-const AccountSelect = ({ address, onSetActiveAccount }: AccountSelectProps) => {
+const AccountSelect = ({ address }: AccountSelectProps) => {
   const accountsMeta = useAccountsMeta()
   const addressMeta = accountsMeta[address]
-
   const activeAccountAddress = useActiveAddress()
   const isCurrentAccount = address === activeAccountAddress
+  const backgroundClient = useBackgroundClient()
+
+  const onSelectAccount = async (address: SuiAddress) => {
+    if (!isCurrentAccount) {
+      await backgroundClient.selectAccount(address)
+    }
+  }
 
   return (
     <div
-      onClick={() => onSetActiveAccount(address)}
+      onClick={() => onSelectAccount(address)}
       className={cl([
         'flex items-center h-[60px] px-4 rounded border cursor-pointer group',
         'transition duration-100 hover:border-[#7db4ff]',
@@ -86,11 +90,7 @@ const WalletManagementPage = () => {
         <div className="flex flex-col grow overflow-y-auto px-6 mx-[-24px]">
           <div className="flex flex-col gap-2">
             {accounts.map((_account) => (
-              <AccountSelect
-                key={_account.address}
-                {..._account}
-                onSetActiveAccount={() => null}
-              />
+              <AccountSelect key={_account.address} {..._account} />
             ))}
             <Button
               className="flex items-center shrink-0 h-[60px] px-4 !rounded border border-[#e2e2e2] transition duration-300 ease-in-out hover:border-[#7db4ff]"
