4674a071c7efc354b3cc3cf731b0cf31c1524c6e
AlexanderZhao <zhaomengzhe@gmail.com>
2023-05-06 15:15:40 +0800

    fix(): update nfts display

--- src/ui/app/pages/nft/components/nft_card.tsx
+++ src/ui/app/pages/nft/components/nft_card.tsx
@@ -5,37 +5,44 @@ import { Loading } from '_app/components'
 
 import { useGetNFTMeta, useFileExtensionType } from '_hooks'
 
-import type { SuiObjectData } from '@mysten/sui.js'
-
 type NftCardProps = {
-  nft: SuiObjectData
+  objectId: string
+  imageOnly?: boolean
   className?: string
 }
 
-const NftCard = ({ nft, className }: NftCardProps) => {
-  const { data: nftMeta, isLoading } = useGetNFTMeta(nft.objectId)
+const NftCard = ({ objectId, imageOnly = false, className }: NftCardProps) => {
+  const { data: nftMeta, isLoading } = useGetNFTMeta(objectId)
 
-  const nftName = nftMeta?.name || formatAddress(nft.objectId)
+  const nftName = nftMeta?.name || formatAddress(objectId)
   const nftImageUrl = nftMeta?.imageUrl || ''
   const fileExtensionType = useFileExtensionType(nftImageUrl)
 
   return (
-    <div className={cl(['flex grow', className])}>
+    <div className={cl(['h-[169px] flex flex-col grow', className])}>
       <Loading loading={isLoading}>
-        {nftImageUrl ? (
-          <img
-            src={
-              nftImageUrl?.replace(/^ipfs:\/\//, 'https://ipfs.io/ipfs/') || ''
-            }
-            alt={nftName || ''}
-            title={nftName}
-            className="w-full"
-          />
-        ) : (
-          <div className="w-full flex justify-center items-center bg-[#eeeeee] text-white text-2xl rounded">
-            {`${fileExtensionType.name} ${fileExtensionType.type}`}
+        <>
+          <div className="w-full flex justify-center items-center bg-[#eeeeee] text-white text-2xl rounded overflow-hidden">
+            {nftImageUrl ? (
+              <img
+                alt={nftName || ''}
+                className="w-[151px] h-[151px] rounded overflow-hidden"
+                height={151}
+                src={
+                  nftImageUrl?.replace(/^ipfs:\/\//, 'https://ipfs.io/ipfs/') ||
+                  ''
+                }
+                title={nftName}
+                width={151}
+              />
+            ) : (
+              <span>{`${fileExtensionType.name} ${fileExtensionType.type}`}</span>
+            )}
           </div>
-        )}
+          {!imageOnly && (
+            <p className="font-bold text-center truncate">{nftName}</p>
+          )}
+        </>
       </Loading>
     </div>
   )
