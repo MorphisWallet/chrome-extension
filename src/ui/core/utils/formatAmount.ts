6f4dfcf36a5b9c5f03f2a8f9d70da44270785fef
AlexanderZhao <zhaomengzhe@gmail.com>
2023-05-06 14:35:20 +0800

    feat(): fork mysten core

--- src/ui/core/utils/formatAmount.ts
+++ src/ui/core/utils/formatAmount.ts
@@ -3,30 +3,38 @@
 
 import BigNumber from 'bignumber.js'
 
-export function formatAmount(
+export function formatAmountParts(
   amount?: BigNumber | bigint | number | string | null
 ) {
   if (typeof amount === 'undefined' || amount === null) {
-    return '--'
+    return ['--']
   }
 
   let postfix = ''
   let bn = new BigNumber(amount.toString())
+  const bnAbs = bn.abs()
 
-  if (bn.gte(1_000_000_000)) {
+  // use absolute value to determine the postfix
+  if (bnAbs.gte(1_000_000_000)) {
     bn = bn.shiftedBy(-9)
-    postfix = ' B'
-  } else if (bn.gte(1_000_000)) {
+    postfix = 'B'
+  } else if (bnAbs.gte(1_000_000)) {
     bn = bn.shiftedBy(-6)
-    postfix = ' M'
-  } else if (bn.gte(10_000)) {
+    postfix = 'M'
+  } else if (bnAbs.gte(10_000)) {
     bn = bn.shiftedBy(-3)
-    postfix = ' K'
+    postfix = 'K'
   }
 
-  if (bn.gte(1)) {
-    bn = bn.decimalPlaces(3, BigNumber.ROUND_DOWN)
+  if (bnAbs.gte(1)) {
+    bn = bn.decimalPlaces(2, BigNumber.ROUND_DOWN)
   }
 
-  return bn.toFormat() + postfix
+  return [bn.toFormat(), postfix]
+}
+
+export function formatAmount(...args: Parameters<typeof formatAmountParts>) {
+  return formatAmountParts(...args)
+    .filter(Boolean)
+    .join(' ')
 }
