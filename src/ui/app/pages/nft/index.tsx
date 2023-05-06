72d9b31966e689ff92f5f219ed4e7dd15e86b93e
AlexanderZhao <zhaomengzhe@gmail.com>
2023-05-06 15:15:40 +0800

    fix(): update nfts display

--- src/ui/app/pages/nft/index.tsx
+++ src/ui/app/pages/nft/index.tsx
@@ -1,36 +1,43 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { getObjectDisplay } from '@mysten/sui.js'
 
 import Layout from '_app/layouts'
 import { Loading, toast } from '_app/components'
 import NftCard from './components/nft_card'
 
-import { useActiveAddress, useObjectsOwnedByAddress } from '_hooks'
+import { useActiveAddress } from '_hooks'
+import { useGetOwnedObjects } from '_src/ui/core/hooks/useGetOwnedObjects'
 
 import type { SuiObjectData } from '@mysten/sui.js'
 
+const MAX_FETCH_LIMIT = 50
+
 const NftPage = () => {
   const accountAddress = useActiveAddress()
-  const { data, isLoading, isError } = useObjectsOwnedByAddress(
+  const { data, isLoading, error, isError } = useGetOwnedObjects(
     accountAddress,
-    { options: { showType: true, showDisplay: true } }
+    {
+      MatchNone: [{ StructType: '0x2::coin::Coin' }],
+    },
+    MAX_FETCH_LIMIT
   )
-  const nfts: SuiObjectData[] =
-    data?.data
-      ?.filter(
-        ({ data }) =>
-          typeof data === 'object' && 'display' in data && data.display
-      )
-      .map(({ data }) => data as SuiObjectData) || []
 
   useEffect(() => {
     if (isError) {
       toast({
         type: 'error',
-        message: 'Sui server error - Failed to load NFTs',
+        message:
+          (error as Error)?.message || 'Sui server error - Failed to load NFTs',
       })
     }
   }, [isError])
+
+  const nfts =
+    data?.pages
+      .flatMap((page) => page.data)
+      .filter((resp) => !!getObjectDisplay(resp).data)
+      .map(({ data }) => data as SuiObjectData) || []
 
   const renderNfts = () => {
     if (!nfts?.length) {
@@ -43,13 +50,13 @@ const NftPage = () => {
 
     return (
       <div className="grid grid-cols-2 gap-x-2.5 gap-y-4 mx-[-24px] mb-[-24px] px-6 pt-2.5 pb-6 overflow-y-auto">
-        {nfts.map((nft) => (
+        {nfts.map((_nft) => (
           <Link
-            to={`./${nft.objectId}`}
-            key={nft.objectId}
-            className="flex h-[152px] rounded bg-[#f0f0f0] overflow-hidden transition-transform duration-100 ease-in-out hover:scale-105"
+            className="flex overflow-hidden transition-transform duration-100 ease-in-out hover:scale-[1.01]"
+            key={_nft.objectId}
+            to={`./${_nft.objectId}`}
           >
-            <NftCard nft={nft} />
+            <NftCard objectId={_nft.objectId} />
           </Link>
         ))}
       </div>
