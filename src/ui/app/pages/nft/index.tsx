0b5717e036e4eed2dee2e6f343b4a286f2b720fa
AlexanderZhao <zhaomengzhe@gmail.com>
2023-05-29 14:24:35 +0800

    feat(): support kiosk NFTs display

--- src/ui/app/pages/nft/index.tsx
+++ src/ui/app/pages/nft/index.tsx
@@ -1,27 +1,16 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { getObjectDisplay } from '@mysten/sui.js'
 
 import Layout from '_app/layouts'
 import { Loading, toast } from '_app/components'
 import NftCard from './components/nft_card'
 
 import { useActiveAddress } from '_hooks'
-import { useGetOwnedObjects } from '_src/ui/core/hooks/useGetOwnedObjects'
-
-import type { SuiObjectData } from '@mysten/sui.js'
-
-const MAX_FETCH_LIMIT = 50
+import { useGetNFTs } from '../../hooks/useGetNFTs'
 
 const NftPage = () => {
   const accountAddress = useActiveAddress()
-  const { data, isLoading, error, isError } = useGetOwnedObjects(
-    accountAddress,
-    {
-      MatchNone: [{ StructType: '0x2::coin::Coin' }],
-    },
-    MAX_FETCH_LIMIT
-  )
+  const { data: nfts, error, isLoading, isError } = useGetNFTs(accountAddress)
 
   useEffect(() => {
     if (isError) {
@@ -32,12 +21,6 @@ const NftPage = () => {
       })
     }
   }, [isError])
-
-  const nfts =
-    data?.pages
-      .flatMap((page) => page.data)
-      .filter((resp) => !!getObjectDisplay(resp).data)
-      .map(({ data }) => data as SuiObjectData) || []
 
   const renderNfts = () => {
     if (!nfts?.length) {
