ea99bf27f34413efb2f41c6352445a7e957e4ef2
AlexanderZhao <zhaomengzhe@gmail.com>
2023-07-03 10:42:54 +0800

    feat(): remove nft pages

--- src/ui/app/layouts/index.tsx
+++ src/ui/app/layouts/index.tsx
@@ -10,7 +10,6 @@ import { useAppSelector, useActiveAddress, useAccountsMeta } from '_hooks'
 
 import CloseIcon from '_assets/icons/close.svg'
 import LandingIcon from '_assets/icons/landing.svg'
-import NftIcon from '_assets/icons/nft.svg'
 import DiscoveryIcon from '_assets/icons/discovery.svg'
 import HistoryIcon from '_assets/icons/history.svg'
 import SettingsIcon from '_assets/icons/settings.svg'
@@ -26,10 +25,6 @@ const ROUTES = [
   {
     name: 'landing',
     icon: LandingIcon,
-  },
-  {
-    name: 'nft',
-    icon: NftIcon,
   },
   {
     name: 'discovery',
@@ -95,7 +90,6 @@ const LayoutBase = ({
               'shrink-0',
               ![
                 '/landing',
-                '/nft',
                 '/discovery',
                 '/history',
                 '/settings',
